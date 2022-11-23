67b19074e48cad8cf032ec5ba3845babdd33e650
Alex <alexsigaevsver@gmail.com>
2022-11-23 15:12:41 +0300

    Добавлены примеры для всех типов антенн. Добавлен график амплитудного распределения в 3D.
    Signed-off-by: Alex <alexsigaevsver@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from antenna_toolbox.PhasedArray import RectangularAntenna, HexagonalAntenna
-from antenna_toolbox.AmplitudeDistributions import ampl_distribution
+from antenna_toolbox.PhasedArray import (
+    RectangularAntenna,
+    HexagonalAntenna,
+    HexagonalRoundAntenna,
+    RectangularRoundAntenna,
+)
+import antenna_toolbox.AmplitudeDistributions as ad
 
 f = 4e9  # Гц
 wavelength = 3e8 / f  # м/c
@@ -13,7 +18,7 @@ b = a
 
 # Шаг решетки по оси x и y в метрах
 dx = 0.9 * wavelength / (1 + np.sin(np.deg2rad(25)))
-dy = 1.5*dx
+dy = dx
 
 dtheta = 0.1
 dphi = 0.1
@@ -21,27 +26,79 @@ dphi = 0.1
 theta = np.deg2rad(np.arange(-90, 90, dtheta))
 phi = np.deg2rad(np.arange(0, 360, dphi))
 
-ant = RectangularAntenna(
-    a=a,
-    b=b,
+
+# ant = RectangularAntenna(
+#     a=a,
+#     b=b,
+#     dx=dx,
+#     dy=dy,
+#     freq=f,
+# )
+
+# ant = HexagonalAntenna(
+#     a=a,
+#     b=b,
+#     ddelta=dx,
+#     freq=f,
+# )
+
+ant = RectangularRoundAntenna(
+    diameter=0.5,
     dx=dx,
     dy=dy,
     freq=f,
 )
 
-# ant2 = HexagonalAntenna(a, b, 0.7*wavelength, f)
+# ant = HexagonalRoundAntenna(
+#     diameter=0.5,
+#     ddelta=dx,
+#     freq=f,
+# )
 
-ant.set_ampl_distribution(ampl_distribution(0.4, 0.4, a, b))
+ant.set_ampl_distribution(ad.ampl_distribution_round(delta=0.4, n=4, a=0.25))
 pd = ant.phase_distribution(np.deg2rad(0), 0)
 
-F = ant.array_factor(theta, np.deg2rad(0), pd)
-# ant.elements_x = ant.elements_x * np.sqrt(3)/2
-# ant.elements_x[::2, :] += np.sqrt(3)/4
-# print(F)
+F = ant.array_factor(theta, np.deg2rad(90), pd)
 
-plt.plot(np.rad2deg(theta), F)
+plt.subplot(2, 1, 1)
+plt.plot(np.rad2deg(theta), ant.array_factor(theta, np.deg2rad(0), pd))
+plt.ylim(-30)
+
+plt.subplot(2, 1, 2)
+plt.plot(np.rad2deg(theta), ant.array_factor(theta, np.deg2rad(90), pd))
 plt.ylim(-30)
 plt.figure(2)
 plt.scatter(ant.elements_x, ant.elements_y)
-plt.show()
 
+fig_4, ax_4 = plt.subplots(subplot_kw={"projection": "3d"})
+
+if len(ant.ampl_distribution.shape) == 2:
+    surf = ax_4.plot_surface(
+        ant.elements_x,
+        ant.elements_y,
+        ant.ampl_distribution,
+        rcount=ant.elements_x.size,
+        ccount=ant.elements_y.size,
+        cmap=plt.get_cmap("rainbow"),
+        linewidth=0,
+        antialiased=False,
+    )
+else:
+    surf = ax_4.plot_trisurf(
+        ant.elements_x,
+        ant.elements_y,
+        ant.ampl_distribution,
+        cmap=plt.get_cmap("rainbow"),
+        linewidth=0,
+        antialiased=False,
+    )
+
+fig_4.colorbar(surf, ticks=np.arange(0, 1.15, 0.05))
+
+ax_4.scatter(ant.elements_x, ant.elements_y, 0)
+ax_4.set_xlabel("$x$, м")
+ax_4.set_ylabel("$Y$, м")
+ax_4.set_zlabel("$A$, В/м")
+
+
+plt.show()
