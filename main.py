ca99e97b43a3d75536ad6e5df857b4b1c25772c0
Alex <alexsigaevsver@gmail.com>
2022-11-28 19:20:54 +0300

    Добавлен анимированный график ДН.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 from antenna_toolbox.PhasedArray import (
     RectangularAntenna,
@@ -60,45 +61,73 @@ pd = ant.phase_distribution(np.deg2rad(0), 0)
 
 F = ant.array_factor(theta, np.deg2rad(90), pd)
 
-plt.subplot(2, 1, 1)
-plt.plot(np.rad2deg(theta), ant.array_factor(theta, np.deg2rad(0), pd))
-plt.ylim(-30)
+# plt.subplot(2, 1, 1)
+# plt.plot(np.rad2deg(theta), ant.array_factor(theta, np.deg2rad(0), pd))
+# plt.ylim(-30)
+# 
+# plt.subplot(2, 1, 2)
+# plt.plot(np.rad2deg(theta), ant.array_factor(theta, np.deg2rad(90), pd))
+# plt.ylim(-30)
+# plt.figure(2)
+# plt.scatter(ant.elements_x, ant.elements_y)
+# 
+# fig_4, ax_4 = plt.subplots(subplot_kw={"projection": "3d"})
+# 
+# if len(ant.ampl_distribution.shape) == 2:
+#     surf = ax_4.plot_surface(
+#         ant.elements_x,
+#         ant.elements_y,
+#         ant.ampl_distribution,
+#         rcount=ant.elements_x.size,
+#         ccount=ant.elements_y.size,
+#         cmap=plt.get_cmap("rainbow"),
+#         linewidth=0,
+#         antialiased=False,
+#     )
+# else:
+#     surf = ax_4.plot_trisurf(
+#         ant.elements_x,
+#         ant.elements_y,
+#         ant.ampl_distribution,
+#         cmap=plt.get_cmap("rainbow"),
+#         linewidth=0,
+#         antialiased=False,
+#     )
+# 
+# fig_4.colorbar(surf, ticks=np.arange(0, 1.15, 0.05))
+# 
+# ax_4.scatter(ant.elements_x, ant.elements_y, 0)
+# ax_4.set_xlabel("$x$, м")
+# ax_4.set_ylabel("$Y$, м")
+# ax_4.set_zlabel("$A$, В/м")
 
-plt.subplot(2, 1, 2)
-plt.plot(np.rad2deg(theta), ant.array_factor(theta, np.deg2rad(90), pd))
-plt.ylim(-30)
-plt.figure(2)
-plt.scatter(ant.elements_x, ant.elements_y)
 
-fig_4, ax_4 = plt.subplots(subplot_kw={"projection": "3d"})
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+# ax.plot(theta, ant.array_factor(theta, np.deg2rad(0), pd))
+# ax.set_rmin(-30)
+# ax.set_rticks(np.arange(-30,1,5))  # Less radial ticks
+# ax.set_rlabel_position(135)  # Move radial labels away from plotted line
+# ax.grid(True)
 
-if len(ant.ampl_distribution.shape) == 2:
-    surf = ax_4.plot_surface(
-        ant.elements_x,
-        ant.elements_y,
-        ant.ampl_distribution,
-        rcount=ant.elements_x.size,
-        ccount=ant.elements_y.size,
-        cmap=plt.get_cmap("rainbow"),
-        linewidth=0,
-        antialiased=False,
-    )
-else:
-    surf = ax_4.plot_trisurf(
-        ant.elements_x,
-        ant.elements_y,
-        ant.ampl_distribution,
-        cmap=plt.get_cmap("rainbow"),
-        linewidth=0,
-        antialiased=False,
-    )
+# x = np.arange(0, 2*np.pi, 0.01)
+line, = ax.plot(theta, ant.array_factor(theta, np.deg2rad(0), pd))
 
-fig_4.colorbar(surf, ticks=np.arange(0, 1.15, 0.05))
 
-ax_4.scatter(ant.elements_x, ant.elements_y, 0)
-ax_4.set_xlabel("$x$, м")
-ax_4.set_ylabel("$Y$, м")
-ax_4.set_zlabel("$A$, В/м")
+def animate(i):
+    
+    theta0 = 45 * np.sin(2*np.pi*i/100)
+    pd = ant.phase_distribution(np.deg2rad(theta0), 0)
+    line.set_ydata(ant.array_factor(theta, np.deg2rad(0), pd))  # update the data.
+    return line,
 
+ax.set_rmin(-30)
+ax.set_rticks(np.arange(-30,1,5))  # Less radial ticks
+ax.set_rlabel_position(135)  # Move radial labels away from plotted line
+ax.grid(True)
+
+ani = animation.FuncAnimation(
+    fig, animate, interval=1, blit=True, save_count=50, frames=100)
+
+ani.save("DN_polar.gif", fps=144)
 
 plt.show()
